fa19a2c50492ab01777787b1af4a2c75c0a84dc5
Tanuj2004 <22egjcs811@gitjaipur.com>
2024-09-20 14:31:13 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,14 @@
+/**
+ * This class contains a set of constant strings used throughout the program
+ * to display messages and show the flow of operations on strings.
+ * Owner -> Tanuj Yadav
+ * Date -> 17/09/2024
+ */
+public final class Constants {
+    public static final String INPUT_ERROR = "Error: Invalid input. Please enter a number between 2 and 8.";
+    public static final String INPUT_ERROR_NUMER =  "Error: Please enter a valid number between 2 and 8.";
+    public static final String GRID_SIZE =  "Enter grid size (2 to 8): ";
+    public static final String DIALOG =  "Game Over! Your score: ";
+    public static final String PLAY_AGAIN =  "\nDo you want to play again?";
+    public static final String GAME_OVER =  "Game Over";
+}
